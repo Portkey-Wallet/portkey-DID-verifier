5f3bd11605646dbff6360314016817c821d30b49
Edward-BXS <Magic.Wizard@portkey.finance>
2024-10-27 12:35:19 +0800

    telegram verify exception

--- src/CAVerifierServer.Grains/Options/JwtTokenOptions.cs
+++ src/CAVerifierServer.Grains/Options/JwtTokenOptions.cs
@@ -1,7 +1,11 @@
 namespace CAVerifierServer.Telegram.Options;
 
+[GenerateSerializer]
+[Alias("CAVerifierServer.Telegram.Options.JwtTokenOptions")]
 public class JwtTokenOptions
 {
+    [Id(0)]
     public string Issuer { get; set; }
+    [Id(1)]
     public IEnumerable<string> Audiences { get; set; }
 }
