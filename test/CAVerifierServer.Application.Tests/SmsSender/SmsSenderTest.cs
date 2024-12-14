19c293d1f047908fb2240e5450e94e9dfdbc03b6
felix-zhaolei <felix@portkey.finance>
2024-12-14 13:00:22 +0800

    modify: deploy.

--- test/CAVerifierServer.Application.Tests/SmsSender/SmsSenderTest.cs
+++ test/CAVerifierServer.Application.Tests/SmsSender/SmsSenderTest.cs
@@ -22,6 +22,7 @@ public partial class SmsSenderTest : CAVerifierServerApplicationTestBase
     private const string FakeOverseaPhoneNum = "+12025550100";
     private const string FakeCode = "123456";
     
+    
     public SmsSenderTest()
     {
         smsServiceSender = GetRequiredService<IEnumerable<ISMSServiceSender>>();
